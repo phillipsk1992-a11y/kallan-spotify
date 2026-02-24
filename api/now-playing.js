b2ac361d8be5a6be3074feb64e9969cda1884a86
phillipsk1992-a11y <phillipsk1992@gmail.com>
2026-02-24 17:52:08 +1200

    Update now-playing.js

--- api/now-playing.js
+++ api/now-playing.js
@@ -5,25 +5,28 @@ module.exports = async function handler(req, res) {
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN } = process.env;
-  const basic = Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64');
+  const clientId = process.env.SPOTIFY_CLIENT_ID;
+  const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+  const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
+  const basic = Buffer.from(clientId + ':' + clientSecret).toString('base64');
 
   const tokenRes = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
     headers: {
-      Authorization: `Basic ${basic}`,
+      'Authorization': 'Basic ' + basic,
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: new URLSearchParams({
       grant_type: 'refresh_token',
-      refresh_token: SPOTIFY_REFRESH_TOKEN,
+      refresh_token: refreshToken,
     }),
   });
 
-  const { access_token } = await tokenRes.json();
+  const tokenData = await tokenRes.json();
+  const accessToken = tokenData.access_token;
 
   const nowRes = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-    headers: { Authorization: `Bearer ${access_token}` },
+    headers: { 'Authorization': 'Bearer ' + accessToken },
   });
 
   if (nowRes.status === 200) {
@@ -32,8 +35,33 @@ module.exports = async function handler(req, res) {
       return res.json({
         isPlaying: data.is_playing,
         track: data.item.name,
-        artist: data.item.artists.map(a => a.name).join(', '),
+        artist: data.item.artists.map(function(a) { return a.name; }).join(', '),
         album: data.item.album.name,
         url: data.item.external_urls.spotify,
         playedAt: null,
-      })
+      });
+    }
+  }
+
+  const recentRes = await fetch('https://api.spotify.com/v1/me/player/recently-played?limit=1', {
+    headers: { 'Authorization': 'Bearer ' + accessToken },
+  });
+
+  if (recentRes.status === 200) {
+    const data = await recentRes.json();
+    if (data.items && data.items.length > 0) {
+      var item = data.items[0];
+      var track = item.track;
+      return res.json({
+        isPlaying: false,
+        track: track.name,
+        artist: track.artists.map(function(a) { return a.name; }).join(', '),
+        album: track.album.name,
+        url: track.external_urls.spotify,
+        playedAt: item.played_at,
+      });
+    }
+  }
+
+  return res.json({ isPlaying: false, track: null, playedAt: null });
+};
